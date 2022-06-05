128a9fefe44728cd420bc2576442857ad97ade3d
Zurek1 <106346038+Zurek1@users.noreply.github.com>
2022-06-05 19:59:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function Destroy() {
+    document.getElementById('front').className = 'DestroyFront';
+    document.getElementById('back').className = 'DestroyBack';
+    document.getElementById('left').className = 'DestroyLeft';
+    document.getElementById('right').className = 'DestroyRight';
+    document.getElementById('top').className = 'DestroyTop';
+    document.getElementById('bottom').className = 'DestroyBottom';
+}
+
+function Restore() {
+    document.getElementById('front').className = 'Front';
+    document.getElementById('back').className = 'Back';
+    document.getElementById('left').className = 'Left';
+    document.getElementById('right').className = 'Right';
+    document.getElementById('top').className = 'Top';
+    document.getElementById('bottom').className = 'Bottom';
+}
+
+function Monitor() {
+    let checkBox = document.getElementById('Destroy');
+    if (checkBox.checked == true) {
+        Destroy();
+    }
+    else {
+        Restore();
+    }
+}
